448948fb2c551567853d686b51b6956b3fb0c0a0
FernandaDeOliveira <pessoal.fernandaoliveira@gmail.com>
2021-09-07 20:55:32 -0300

    STarting to build the Room DB

--- app/src/main/java/com/example/teste/App.kt
+++ app/src/main/java/com/example/teste/App.kt
@@ -1,6 +1,10 @@
 package com.example.teste
 
 import android.app.Application
+import com.example.teste.data.AppDataBase
+import com.example.teste.data.BusinessCardRepository
 
 class App : Application() {
+    val database by lazy { AppDataBase.getDataBase(this) }
+    val repository by lazy { BusinessCardRepository(database.businessDao()) }
 }
