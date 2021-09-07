1bfab2e9d88037b28a98d7bf3b8df9afbdfb9272
FernandaDeOliveira <pessoal.fernandaoliveira@gmail.com>
2021-09-07 20:22:42 -0300

    Creating layout for adding a new card and it's basic functions

--- app/src/main/java/com/example/teste/ui/MainActivity.kt
+++ app/src/main/java/com/example/teste/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.teste.ui
 
 import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.teste.R
@@ -13,5 +14,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        insertListener()
+    }
+
+    private fun insertListener(){
+        binding.addCard.setOnClickListener {
+            //isso aqui é uma intencao, diz que ao clicar no botao da main vai puxar a de add
+            val intent = Intent(this@MainActivity, AddBusinessCardActivity ::class.java)
+            startActivity(intent)
+        }
     }
 }
